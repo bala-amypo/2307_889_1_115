f870252766a6e9366620d1ac3c361a8d9ed42ada
bala-amypo <bala@amypo.in>
2025-12-21 13:48:39 +0000

    User commit

--- src/main/java/com/example/demo/dto/LoginRequest.java
+++ src/main/java/com/example/demo/dto/LoginRequest.java
@@ -4,12 +4,3 @@ public class LoginRequest {
     public void setEmail(String email) {}
     public void setPassword(String password) {}
 }
-
-public class RegisterRequest {
-    [cite_start]private String fullName; [cite: 128]
-    [cite_start]private String email; [cite: 129]
-    [cite_start]private String password; [cite: 130]
-    public void setFullName(String fullName) {}
-    public void setEmail(String email) {}
-    public void setPassword(String password) {}
-}
