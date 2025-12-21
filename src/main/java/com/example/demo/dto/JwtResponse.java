a578f369738d10135cee654326e64782ecb3850e
bala-amypo <bala@amypo.in>
2025-12-21 14:55:35 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/JwtResponse.java
@@ -0,0 +1,42 @@
+package com.example.demo.dto;
+
+public class JwtResponse {
+    private String token;
+    private Long userId;
+    private String email;
+    private String role;
+
+    public JwtResponse() {
+    }
+
+    public JwtResponse(String token, Long userId, String email, String role) {
+    }
+
+    public String getToken() {
+        return null;
+    }
+
+    public void setToken(String token) {
+    }
+
+    public Long getUserId() {
+        return null;
+    }
+
+    public void setUserId(Long userId) {
+    }
+
+    public String getEmail() {
+        return null;
+    }
+
+    public void setEmail(String email) {
+    }
+
+    public String getRole() {
+        return null;
+    }
+
+    public void setRole(String role) {
+    }
+}
