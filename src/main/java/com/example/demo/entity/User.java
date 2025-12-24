dc0e3e511f9243982822b123cb891754b63a92ff
bala-amypo <bala@amypo.in>
2025-12-24 15:49:09 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -17,16 +17,14 @@ public class User {
     private String email;
 
     private String password;
+
     private String role;
+
     private LocalDateTime createdAt;
 
-    @PrePersist
-    public void prePersist() {
-        if (role == null) role = "MONITOR";
-        createdAt = LocalDateTime.now();
+    public User() {
     }
 
-    public User() {}
 
     public User(Long id, String fullName, String email, String password, String role) {
         this.id = id;
@@ -36,12 +34,62 @@ public class User {
         this.role = role;
     }
 
-    public Long getId() { return id; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+   
+    @PrePersist
+    public void prePersist() {
+        if (this.role == null) {
+            this.role = "MONITOR";
+        }
+        this.createdAt = LocalDateTime.now();
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    
 
-    public String getRole() { return role; }
+    public Long getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
